3bb992f337bac2dd1e342d538391b9e65bb7ddcd
remeessalam <remeesreme4u@gmail.com>
2024-09-21 23:32:16 +0530

    small w and h change in story

--- insta-chat_frontEnd/src/components/home/child-components/Story.jsx
+++ insta-chat_frontEnd/src/components/home/child-components/Story.jsx
@@ -1,13 +1,13 @@
 const Story = () => {
   return (
-    <div className="flex  h-28 w-full  rounded-md  cursor-not-allowed ">
-      <div className="flex justify-start overflow-x-auto scrollbar-hide gap-1 mt-2 ml-2">
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+    <div className="flex  h-[135px] w-full  rounded-md  cursor-not-allowed ">
+      <div className="flex justify-start overflow-x-auto scrollbar-hide mt-2 ml-2">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
@@ -15,205 +15,207 @@ const Story = () => {
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
           <h1 className="truncate  h-8 font-normal text-xs">WORKING</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
-          <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
+          <h1 className="truncate h-8 font-normal text-xs text-center">
+            remees
+          </h1>
           <h1 className="truncate  h-8 font-normal text-xs">ON IT</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
           </div>
           <h1 className="truncate h-8 font-normal text-xs">remees salam</h1>
         </div>
-        <div className="flex flex-col w-[66px] m-1">
-          <div className="border-2 rounded-full flex justify-center w-[58px] h-[58px] border-red-400">
+        <div className="flex flex-col w-[90px] m-1">
+          <div className="border-[3px] rounded-full flex justify-center w-[90px] h-[90px] border-red-400">
             <img
-              width={56}
-              height={56}
-              className="rounded-full aspect-square border-2 border-white"
+              width={90}
+              height={90}
+              className="rounded-full aspect-square border-[3px] border-white"
               src="https://res.cloudinary.com/domqbgvw6/image/upload/v1669275021/tether/tklrjgkrbuvmpoonswvg.png"
               alt=""
             />
